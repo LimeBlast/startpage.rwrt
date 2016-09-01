96951d738313155dac73751f1f5bb7f2ecb30ffb
Daniel Hollands <daniel@limeblast.co.uk>
2016-09-01 07:55:53 +0100

    Update settings to remove search engines and added icons

--- js/script.js
+++ js/script.js
@@ -36,19 +36,19 @@
 \*==========*/
 var settings = {
 	"navigation": {
-		"newWindow": true
+		"newWindow": false
 	},
 	
 	"search": {
 		"engines": [
-			["http://www.google.com/search", "q", "Google", "sg"],
-			["http://www.google.com/images", "q", "Google Images", "si"],
-			["http://search.yahoo.com/search", "p", "Yahoo", "sy"],
-			["http://wikipedia.org/w/index.php", "w", "Wikipedia", "sw"],
-			["http://www.dict.cc", "s", "dict.cc", "sd"],
-			["http://dict.leo.org", "search", "leo", "sl"],
-			["http://www.flickr.com/search", "q", "flickr", "sf"],
-			["http://browse.deviantart.com/", "q", "deviantArt", "sa"]
+			// ["http://www.google.com/search", "q", "Google", "sg"],
+			// ["http://www.google.com/images", "q", "Google Images", "si"],
+			// ["http://search.yahoo.com/search", "p", "Yahoo", "sy"],
+			// ["http://wikipedia.org/w/index.php", "w", "Wikipedia", "sw"],
+			// ["http://www.dict.cc", "s", "dict.cc", "sd"],
+			// ["http://dict.leo.org", "search", "leo", "sl"],
+			// ["http://www.flickr.com/search", "q", "flickr", "sf"],
+			// ["http://browse.deviantart.com/", "q", "deviantArt", "sa"]
 		],
 		"focusSearch": false
 	},
@@ -58,7 +58,7 @@ var settings = {
 	},
 
 	"animation": {
-		"hideLinks": true
+		"hideLinks": false
 	},
 
 	"icons": {
